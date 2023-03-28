4e9bf1fa900d6de28367fbd3cf6c66cb6e9f1b1a
Nagabhushan S N (Lab PC) <nagabhushans@iisc.ac.in>
2023-03-28 23:42:12 +0530

    QA code added.

---
+++ src/QA/00_Common/src/CommonUtils.py
@@ -0,0 +1,19 @@
+# Shree KRISHNya Namaha
+# Some common utilities
+# Author: Nagabhushan S N
+# Last Modified: 04/10/2021
+
+import json
+from pathlib import Path
+from typing import List
+
+import pandas
+
+
+def start_matlab_engine():
+    import matlab.engine
+
+    print('Starting MatLab Engine')
+    matlab_engine = matlab.engine.start_matlab()
+    print('MatLab Engine active')
+    return matlab_engine
